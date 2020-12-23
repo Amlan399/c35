114691f798105bbba1939e49191f7be72f2b251f
Amlan399 <69415024+Amlan399@users.noreply.github.com>
2020-12-23 16:13:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var database;
+var backgroundImage;
+var gameState = 0;
+var playerCount;
+var form;
+var player;
+var game;
+
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game = new Game()
+  game.getState();
+  game.start()
+
+    
+
+ 
+}
+
+function draw(){
+ 
+  
+}
+
+
